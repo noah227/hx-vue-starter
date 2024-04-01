0e1815651f276c6742f05e945cfdeab83d6e37d9
noah227 <noah227@foxmail.com>
2024-04-01 17:01:08 +0800

    chore: 升级cozip，补全配置中缺失的项

--- hx-pack.js
+++ hx-pack.js
@@ -2,10 +2,12 @@ const cozip = require("cozip")
 
 const pack = () => {
     cozip("pack.zip", [
+        // [path, isDir, filter?]
         ["./src/html/dist", true],
         ["./src/html.js"],
         ["./src/main.js"],
         ["./src/utils.js"],
+        ["./extension.js"],
         ["./package.json"],
         ["./node_modules/hx-vue-dist-loader", true],
     ])
