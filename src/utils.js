4e13cd5a479f0241e92c2d30fc3c64e8f62f90e0
noah227 <noah227@foxmail.com>
2024-11-06 15:24:33 +0800

    chore: 添加参考文档链接；优化utils中路径的处理

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 const hx = require("hbuilderx")
+const path = require("path")
 
 const _configSets = [
     ["colorScheme", "editor.colorScheme"]
@@ -6,6 +7,7 @@ const _configSets = [
 module.exports = {
     /**
      * @param {[k: string, gk: string][]} configSets
+     * @see https://hx.dcloud.net.cn/ExtensionDocs/Api/workspace/getConfiguration?id=get
      */
     getConfiguration(configSets=[]) {
         const config = hx.workspace.getConfiguration()
@@ -17,13 +19,15 @@ module.exports = {
     },
     /**
      * 更新配置
-     * 
+     *
      * 更多关于配置获取与保存的注意事项，见README中有关的说明
+     *
+     * @see https://hx.dcloud.net.cn/ExtensionDocs/Api/workspace/getConfiguration?id=update
      * @param {Object} obj
      * @param {string} section !!这里默认使用了插件的id为主key，如有需要请自行更改
      */
-    updateConfiguration(obj={}, section){ 
-        section = section || require("../package.json").id
+    updateConfiguration(obj={}, section){
+        section = section || require(path.resolve(__dirname, "../package.json")).id
         const config = hx.workspace.getConfiguration(section)
         Object.entries(obj).forEach(([k, v]) => {
             config.update(k, v)
